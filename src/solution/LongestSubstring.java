efb72c4828a38088f8529ebe6fd6f29cc38d765a
Oliver388 <oliverling12345@gmail.com>
2022-07-01 13:07:49 +0800

    second commit

--- src/solution/LongestSubstring.java
+++ src/solution/LongestSubstring.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 
 public class LongestSubstring {
 
+    /**
+     * 本质上就是维护一个list集合，如果新加入集合的字符之前出现过，那么就从头去掉元素直至重复的那个字符
+     * 时间复杂度: O(N),便利一遍字符串
+     * 空间复杂度：O(字符集大小)
+     * @param s
+     * @return
+     */
     public int lengthOfLongestSubstring(String s) {
         int max = 0;
         ArrayList<Character> temp = new ArrayList<Character>();
@@ -26,6 +33,7 @@ public class LongestSubstring {
     }
 
     public static void main(String[] args) {
-
+        LongestSubstring longestSubstring = new LongestSubstring();
+        System.out.println();
     }
 }
