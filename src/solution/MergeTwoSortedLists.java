9ccd8308bc776d29c045528f8b231e292a63da9f
Oliver388 <oliverling12345@gmail.com>
2022-07-09 21:07:03 +0800

    eighth commit

--- src/solution/MergeTwoSortedLists.java
+++ src/solution/MergeTwoSortedLists.java
@@ -1,10 +1,40 @@
 package solution;
 
 /**
+ * @id 21
  * @author linglifan
  * @date 2022/07/09 13:31
  */
 public class MergeTwoSortedLists {
+    /**
+     * 递归(递归真的不会)
+     * 时间复杂度：O(n + m)
+     * 空间复杂度：O(n + m)
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public ListNode mergeTwoListsByRecursive(ListNode list1, ListNode list2){
+        if (list1 == null){
+            return list2;
+        }else if (list2 == null){
+            return list1;
+        }else if (list1.val < list2.val){
+            list1.next = mergeTwoListsByRecursive(list1.next, list2);
+            return list1;
+        }else{
+            list2.next = mergeTwoListsByRecursive(list1,list2.next);
+            return list2;
+        }
+    }
+    /**
+     * 迭代
+     * 时间复杂度：O(n+m)
+     * 空间复杂度: O(1)
+     * @param list1
+     * @param list2
+     * @return
+     */
     public ListNode mergeTwoListsByIterate(ListNode list1, ListNode list2) {
         ListNode merge = new ListNode(0);
         ListNode prev = merge;
